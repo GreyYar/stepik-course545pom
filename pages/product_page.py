4a7672eeccada262e6155e3fd455e01ea1795452
Sergey Spasskiy <sergey.spasskiy@akvelon.com>
2019-07-05 17:27:22 +0300

    Add methods for Product Page POM: Get Item name\price

--- pages/product_page.py
+++ pages/product_page.py
@@ -18,16 +18,26 @@ class ProductPage(BasePage):
         except NoAlertPresentException:
             print("No second alert presented")
 
+    def get_item_name(self):
+        assert self.is_element_present(*ProductPageLocators.ITEM_NAME), \
+            "Item name is not presented"
+        return self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+
+    def get_item_price(self):
+        assert self.is_element_present(*ProductPageLocators.ITEM_PRICE), \
+            "Item price is not presented"
+        return self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+
     def add_item_to_basket(self):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
 
     def should_be_item_added_to_card_msg(self):
-        book_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        book_name = self.get_item_name()
         added_book = self.browser.find_element(*ProductPageLocators.ITEM_ADDED_TO_CART).text
         assert added_book == book_name, "Book added to cart differs from current book name"
 
     def cart_price_msg_equals_item_price(self):
-        item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+        item_price = self.get_item_price()
         cart_total = self.browser.find_element(*ProductPageLocators.CART_TOTAL_PRICE).text
         assert item_price == cart_total, "Cart total is differs from Item price"
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
 def test_guest_can_add_product_to_cart(browser):
